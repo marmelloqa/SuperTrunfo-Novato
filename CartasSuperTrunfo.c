476bb104a3a151224fc9af50bbaaa3d778bdb74e
Rafael QA <202510132056@alunos.estacio.br>
2025-10-29 22:16:31 +0000

    definição de variáveis cartas 1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,29 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // --- Definição de variáveis para a Carta 1 ---
+
+    char estado1;           
+    char codigo1[4];        
+    char nome1[50];         
+    int populacao1;         
+    float area1;            
+    float pib1;             
+    int pontosTuristicos1;  
+
+  // --- Definição de variáveis para a Carta 2 ---
+  
+    char estado2;
+    char codigo2[4];
+    char nome2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+
   // Área para entrada de dados
+
+
 
   // Área para exibição dos dados da cidade
 
